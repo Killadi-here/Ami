cd3e9a4fdf4b73245961069581239556d0b3228f
KILLADI <118595331+Killadi-here@users.noreply.github.com>
2024-04-23 20:27:06 +0530

    Update yui_msgs.py

--- Yui/data/yui_msgs.py
+++ Yui/data/yui_msgs.py
@@ -1,40 +1,40 @@
-# Copyright (c) 2021 Itz-fork
-
 Emergency_Msgs = [
-    "I'm not feeling well. I think i should go and sleep now.",
-    "I have some work to do. Bye!",
-    "I'm very tired. I need to take a nap.",
-    "Ooops!, I think my owner forgot to pay my bill!",
-    "Oh, I think something happened to me!",
-    "Nice to meet you but I have to go now, Bye!"
+    "Eda mwone",
+    "kukkuduu",
+    "vaavee",
+    "mwolee",
+    "moye moye",
 ]
 
 Photo_Reesponse = [
-    "Woah, that's a nice picture",
-    "Hmmm... This one looks prettry good!",
-    "What's this?",
-    "IMHO This is the worst image I've ever seen in my life",
-    "Pathetic...",
-    "Ok? I guess",
-    "I don't like photos :(",
-    "This image is weird",
-    "I don't know what to do with this image",
-    "Hey, can we please chat only using text messages? because I can't understand photos correctly :("
+    "enth alinja photo aahn da",
+    "sundharaaa",
+    "poda galla",
+    "shyoo kastam thanne",
+    "kanne karalee",
+    "amfeda maname",
+    "shammi hero da",
+    "ninne kaanumpol enk appurathe vtle pattiye orna vann",
+    "ithilum bedham ente poocha arnnu",
+    "look like a moneky"
 ]
 
 Sticker_Response = [
-    "Aww, nice sticker",
-    "I like this sticker ngl.",
-    "Who made this?",
-    "This sticker looks terrible.",
-    "Uhmmm... So what's up with this sticker?",
-    "Ok, cool",
-    "I love these types of stickers",
-    "Can we please chat only using text messages? because I can't understand stickers correctly :("
+    "mwoleee",
+    "mwoneee",
+    "nee oru mandan thanne",
+    "i lub you",
+    "please die",
+    "sry,I don't need you",
+    "kanji veno manikya",
+    "poda paal kuppi",
+    "nee pottan ayt jeevikuka ahno",
+    "onn poi theravo",
+    "malare ninne"
 ]
 
 Video_and_gif_Response = [
-    "What is this?",
+    "ithenth menga",
     "I don't wanna waste my data to download this file",
     "What do you want?",
     "Can you elborate?",
